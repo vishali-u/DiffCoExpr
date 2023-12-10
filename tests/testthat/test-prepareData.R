2b340dfcbd1952837477f029125ee4cf14afdec4
Vishali Umaiyalan <vishali.umaiyalan@mail.utoronto.ca>
2023-12-10 00:12:20 -0500

    added tests for prepareData

--- tests/testthat/test-prepareData.R
+++ tests/testthat/test-prepareData.R
@@ -2,12 +2,31 @@ library(DiffCoExpr)
 
 test_that("valid input", {
   srat <- prepareData(
-    gene_bc_matrix_path = "inst/extdata/pbmc/filtered_gene_bc_matrices",
-    cell_types_path = "inst/extdata/pbmc/cell_types.csv"
+    geneMatrixPath = system.file("extdata",
+                                 "filtered_gene_bc_matrices",
+                                 package =  "DiffCoExpr"),
+    cellTypesPath = system.file("extdata",
+                                "cell_types.csv",
+                                package =  "DiffCoExpr")
   )
   
-  #expect_type(srat, "Seurat")
   expect_identical(levels(srat), 
                    c("Naive CD4+ T", "CD14+ Mono", "Memory CD4+", "B", 
                      "CD8+ T", "FCGR3A+ Mono", "NK", "DC", "Platelet"))
 })
+
+test_that("invalid input", {
+  
+  # Gene matrix path is invalid
+  expect_error(prepareData(geneMatrixPath = "",
+                           cellTypesPath = NULL))
+  
+  # Cell types path is invalid (does not exist and is not NULL)
+  expect_error(prepareData(geneMatrixPath = 
+                             system.file("extdata",
+                                         "filtered_gene_bc_matrices",
+                                          package =  "DiffCoExpr"), 
+                           cellTypesPath = ""))
+})
+
+
